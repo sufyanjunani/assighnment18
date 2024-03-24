b917b245a268737a4951b5d6c6e9119701abe3ec
sufyan junani <sasifjunani@gmail.com>
2024-03-24 06:13:47 +0500

    add assighnment

---
+++ app.js
@@ -0,0 +1,55 @@
+//             // q
+// // var allLower = userInput.toLowerCase();
+//             // q
+// // x = x.toLowerCase();
+//             q
+// y = y.toUpperCase();
+//             q
+// let lowerCaseVariable = yourVariable.toLowerCase();
+//             q
+// let newVariable = yourArray[index].toLowerCase();
+//             q
+// var yourVariable = "Your string"; // Replace "Your string" with your actual string
+// var upperCaseString = yourVariable.toUpperCase();
+// alert(upperCaseString);
+//             q
+// var cityName = "kaRacHi";
+// var capitalisedCityName = cityName.charAt(0).toUpperCase() + cityName.slice(1).toLowerCase();
+// console.log(capitalisedCityName); // Output: Karachi
+//         chapter 22 - 25 (Strings)
+//             q
+// var cityName = "kaRacHi";
+// var numberOfCharacters = cityName.length;
+// console.log(numberOfCharacters); // Output: 7
+//             q
+// var animal = "elephant";
+// var seg = animal.slice(1, 5); // Slicing from index 1 to index 4 (exclusive)
+// console.log(seg); // Output: leph
+//             q
+// var myString = "example"; // Replace "example" with your string
+// var numberOfCharacters = myString.length;
+// console.log(numberOfCharacters); // Output: 7
+//             q
+// var myString = "exampleString"; // Replace "exampleString" with your string
+// var lengthOfString = myString.length;
+// console.log("Length of the string:", lengthOfString); // Output: Length of the string: 14
+// var slicedString = myString.slice(1, -3);
+// console.log("Sliced string:", slicedString); // Output: Sliced string: xampleStr
+//             q
+// indx = 16
+//             q
+// var input = "exampleString"; // Assuming input is already defined
+// var cha = input[4];
+// console.log("Fifth character:", cha); // Output: Fifth character: p
+//             q
+// var str = "exampleString"; // Assuming str is already defined
+// var x = str[str.length - 1];
+// console.log("Last character:", x); // Output: Last character: g
+//             q
+// var str = "abcde";
+// var characterAtIndex2 = str.charAt(2);
+// console.log("Character at index 2:", characterAtIndex2); // Output: Character at index 2: c
+//             q
+// var text = "Lorem ipsum";
+// var cha = text[9];
+// console.log("10th character:", cha); // Output: 10th character: i
